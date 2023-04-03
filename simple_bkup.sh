87fb44322f722113a225f4281824f57711e9e4e1
Adam <ageodru@gmail.com>
2023-04-03 17:24:08 -0400

    deprecating timeshift

--- simple_bkup.sh
+++ simple_bkup.sh
@@ -6,19 +6,19 @@
 # Currently, being manually backed up to Google Drive
 # --> Can now use `gupload` (Jan 2021)
 # removed /etc/passwd from backup
-
+# deprecating timeshift bits on new fedora laptop as timeshift caused a whole lot of fuckery and i am not doing it anymore (Apr 2023)
 
 TODAY=$(date +%d-%m-%Y)
 
 BACKUPDIR="/home/$USER/Backups/"
-SNAPSHOTDIR="/home/timeshift/snapshots/"
+#SNAPSHOTDIR="/home/timeshift/snapshots/"
 NEWEST=$(ls -t $BACKUPDIR | head -n +1)
 
 # Count items in backup directory
 COUNT=$(ls $BACKUPDIR -t | wc -l)
 
 # Count items in snapshot dir
-SCOUNT=$(ls $SNAPSHOTDIR -t | wc -l)
+#SCOUNT=$(ls $SNAPSHOTDIR -t | wc -l)
 
 # Create tarball from specified directories with exclusions
 back_up_files() {
@@ -35,12 +35,12 @@ back_up_dir() {
 
 # clean up the snapshot dir
 # this needs sudo i think
-snap_clean() {
-	if [ $SCOUNT -gt "2" ]; then
-		cd $SNAPSHOTDIR
-		ls -t $SNAPSHOTDIR | tail -n +3 | xargs rm -rf
-	fi
-}
+#snap_clean() {
+#	if [ $SCOUNT -gt "2" ]; then
+#		cd $SNAPSHOTDIR
+#		ls -t $SNAPSHOTDIR | tail -n +3 | xargs rm -rf
+#	fi
+#}
 
 # Check the specified backup dir exists, if not, create it
 test -d $BACKUPDIR
@@ -50,7 +50,7 @@ fi
 
 back_up_files
 back_up_dir
-snap_clean
+#snap_clean
 
 #---------------------------------
 
